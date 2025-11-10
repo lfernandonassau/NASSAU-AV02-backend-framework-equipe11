d1940ae69410d12c7fd15d02ab9c460bc1e45e30
IvanDev404 <pvpivan42@gmail.com>
2025-11-10 18:29:22 -0300

    criando o app e definindo o banco de dados

--- backend_financeair/financeair/settings.py
+++ backend_financeair/financeair/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_financeair',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,11 @@ WSGI_APPLICATION = 'financeair.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'financeairdb',
+        'USER': 'postgres',
+        'PASSWORD': 122989,
+        'HOST': 'localhost',
     }
 }
 
