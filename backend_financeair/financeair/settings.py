4a8a11599c3411c701974b08c8ae08ebaa750532
IvanDev404 <pvpivan42@gmail.com>
2025-11-10 20:00:50 -0300

    adicionando o simpleJWT e o Rest framework

--- backend_financeair/financeair/settings.py
+++ backend_financeair/financeair/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_financeair',
+    'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'financeairdb',
-        'USER': 'postgres',
-        'PASSWORD': 122989,
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
         'HOST': 'localhost',
     }
 }
@@ -101,6 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+  'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+  ),
+  'DEFAULT_PERMISSION_CLASSES': (
+    'rest_framework.permissions.IsAuthenticated',
+  ),
+}
 
 
 # Internationalization
