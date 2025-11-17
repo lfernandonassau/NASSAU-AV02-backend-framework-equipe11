12c8e7a8cd0477d7dd0056d5995e68df65316fa0
IvanDev404 <pvpivan42@gmail.com>
2025-11-17 19:23:38 -0300

    ajustando parametros do app

--- core/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .model import Conta, Categoria, TipoTransacao, Transacao, MetaFinanceira, DespesaFixa, FonteRenda
-
-admin.site.register({Conta, Categoria, TipoTransacao, TipoTransacao, Transacao, MetaFinanceira, DespesaFixa, FonteRenda })
